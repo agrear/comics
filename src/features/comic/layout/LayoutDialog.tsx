af26ad0522a48494566ac6c2b166f7643303c7a2
agrear <malte.wassmann@hotmail.de>
2022-03-05 23:52:35 +0100

    Optimize layout picker

--- src/features/comic/layout/LayoutDialog.tsx
+++ src/features/comic/layout/LayoutDialog.tsx
@@ -1,52 +1,100 @@
-import CloseIcon from '@mui/icons-material/Close';
 import SettingsOverscanIcon from '@mui/icons-material/SettingsOverscan';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import IconButton from '@mui/material/IconButton';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-import LayoutForm from './LayoutForm';
-import { Comic } from '../comicSlice';
+import LayoutPositionPicker from './LayoutPositionPicker';
+import { Comic, comicLayoutUpdated } from '../comicSlice';
+import { Layout, ObjectFit } from 'utils';
 
 interface LayoutDialogProps {
   comic: Comic;
 }
 
 export function LayoutDialog({ comic }: LayoutDialogProps) {
-  const [isOpen, setOpen] = React.useState(false);
+  const dispatch = useDispatch();
+
+  const [open, setOpen] = React.useState(false);
 
   const handleClose = () => {
     setOpen(false);
   };
 
+  const handleLayoutChange = (layout: Partial<Layout>) => {
+    dispatch(comicLayoutUpdated({
+      comicId: comic.id,
+      layout: {
+        ...comic.layout,
+        ...layout
+      }
+    }));
+  };
+
   return (
     <>
-      <Button startIcon={<SettingsOverscanIcon />} onClick={() => setOpen(true)}>
+      <Button
+        startIcon={<SettingsOverscanIcon />}
+        onClick={() => setOpen(true)}
+      >
         Layout
       </Button>
 
       <Dialog
-        open={isOpen}
+        open={open}
         onClose={handleClose}
         maxWidth="sm"
         fullWidth
-        sx={{ userSelect: "none" }}
+        sx={{ userSelect: 'none' }}
       >
         <DialogTitle>Page Layout</DialogTitle>
 
-        <IconButton
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            top: 1,
-            right: 1
-          }}
+        <DialogContent
+          sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}
         >
-          <CloseIcon sx={{ fontSize: 'medium' }} />
-        </IconButton>
-
-        <LayoutForm comicId={comic.id} layout={comic.layout} onSubmit={handleClose} />
+          <Box sx={{ display: 'flex', my: 3, py: 2 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Fit</FormLabel>
+                <RadioGroup
+                  name="fit"
+                  value={comic.layout.fit}
+                  onChange={(_event, value) => handleLayoutChange({
+                    fit: value as ObjectFit
+                  })}
+                >
+                  <FormControlLabel value="none" control={<Radio />} label="None" />
+                  <FormControlLabel value="contain" control={<Radio />} label="Contain" />
+                  <FormControlLabel value="cover" control={<Radio />} label="Cover" />
+                  <FormControlLabel value="fill" control={<Radio />} label="Fill" />
+                  <FormControlLabel value="scale-down" control={<Radio />} label="Scale down" />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexGrow: 1
+              }}
+            >
+              <LayoutPositionPicker
+                fit={comic.layout.fit}
+                position={comic.layout.position}
+                onChange={position => handleLayoutChange({ position })}
+              />
+            </Box>
+          </Box>
+        </DialogContent>
       </Dialog>
     </>
   );
---
+++ src/features/comic/layout/LayoutPositionPicker.tsx
@@ -0,0 +1,93 @@
+import ImageIcon from '@mui/icons-material/ImageSharp';
+import Box from '@mui/material/Box';
+import { motion, MotionStyle } from 'framer-motion';
+import React from 'react';
+
+import {
+  fitObjectSize,
+  getHorizontalAlignment,
+  getVerticalAlignment,
+  ObjectFit,
+  ObjectPosition
+} from 'utils';
+
+type Point = { position: ObjectPosition, style: MotionStyle };
+const pointOverlap = 8;
+const positionPoints: Point[] = [
+  { position: "left top", style: { top: -pointOverlap, left: -pointOverlap } },
+  { position: "center top", style: { top: -pointOverlap, left: `calc(50% - ${pointOverlap}px)` } },
+  { position: "right top", style: { top: -pointOverlap, right: -pointOverlap } },
+  { position: "left center", style: { top: `calc(50% - ${pointOverlap}px)`, left: -pointOverlap } },
+  { position: "center center", style: { top: `calc(50% - ${pointOverlap}px)`, left: `calc(50% - ${pointOverlap}px)` } },
+  { position: "right center", style: { top: `calc(50% - ${pointOverlap}px)`, right: -pointOverlap } },
+  { position: "left bottom", style: { bottom: -pointOverlap, left: -pointOverlap } },
+  { position: "center bottom", style: { bottom: -pointOverlap, left: `calc(50% - ${pointOverlap}px)` } },
+  { position: "right bottom", style: { bottom: -pointOverlap, right: -pointOverlap } }
+];
+
+interface LayoutPositionPickerProps {
+  fit: ObjectFit;
+  position: ObjectPosition;
+  onChange: (position: ObjectPosition) => void;
+}
+
+function LayoutPositionPicker({
+  fit,
+  position,
+  onChange
+}: LayoutPositionPickerProps) {
+  const container = { width: 180, height: 140 };
+  const image = { width: 80, height: 100 };
+  const size = fitObjectSize(fit, container, image);
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'flex',
+        border: 2,
+        borderColor: 'white',
+        ...container,
+        alignItems: getVerticalAlignment(position),
+        justifyContent: getHorizontalAlignment(position)
+      }}
+    >
+      <Box
+        component={motion.div}
+        layout
+        sx={{
+          ...size,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          border: 1,
+          borderColor: 'white',
+          opacity: 0.5,
+          pointerEvents: 'none',
+          zIndex: 'tooltip'
+        }}
+      >
+        <ImageIcon />
+      </Box>
+      
+      {positionPoints.map(({ position: pos, style }) => (
+        <motion.div
+          key={pos}
+          style={{
+            ...style,
+            backgroundColor: pos === position ? 'red' : 'white',
+            position: 'absolute',
+            width: 16,
+            height: 16,
+            borderRadius: '50%',
+            cursor: pos === position ? 'default' : 'pointer'
+          }}
+          whileHover={{ scale: 1.25 }}
+          onClick={() => onChange(pos)}
+        />
+      ))}
+    </Box>
+  );
+}
+
+export default LayoutPositionPicker;
